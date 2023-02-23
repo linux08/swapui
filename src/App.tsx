eec699d2cc4eaab068497983968bcf7ede05cf0a
David Abimbola <abimbola130@gmail.com>
2023-02-23 00:48:53 -0600

    chore: basic component set up

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+
+import {
+  Flex,
+  Box,
+  Text,
+} from "@chakra-ui/react";
+import InputComp from './components/Input';
+import { SettingsIcon } from '@chakra-ui/icons';
 import './App.css';
 
 function App() {
+
+  const [value, setValue] = useState<string>("0");
+
+  function handleInputChange(newValue: string) {
+    setValue(newValue);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Box
+        w="30.62rem"
+        mx="auto"
+        pt="15.25rem"
+        boxShadow="rgb(0 0 0 / 8%) 0rem 0.37rem 0.62rem"
+        borderRadius="1.37rem">
+
+        <Flex
+          alignItems="center"
+          p="1rem 1.25rem 0.5rem"
+          bg="white"
+          color="rgb(86, 90, 105)"
+          justifyContent="space-between"
+          borderRadius="1.37rem 1.37rem 0 0">
+          <Text
+            color="black"
+            fontWeight="500">
+            Swap
+          </Text>
+          <SettingsIcon
+            fontSize="1.25rem"
+            cursor="pointer"
+            _hover={{ color: "rgb(128,128,128)" }}
+          />
+        </Flex>
+
+        <Box
+          p="0.5rem"
+          bg="white"
+          borderRadius="0 0 1.37rem 1.37rem">
+
+          <InputComp
+            tokenName="USDC"
+            value={value}
+            onChangeInput={handleInputChange}
+          />
+
+          <InputComp
+            tokenName="COMP"
+            value={(parseInt(value) * 10).toString()}
+          />
+
+        </Box>
+      </Box>
     </div>
   );
 }
---
+++ src/components/Input.tsx
@@ -0,0 +1,65 @@
+import {
+  Flex,
+  Box,
+  Text,
+  Input
+} from "@chakra-ui/react";
+
+interface InputProps {
+  tokenName: string;
+  value: string;
+  onChangeInput?: (newValue: string) => void;
+}
+
+
+export default function InputComp({ tokenName, value, onChangeInput }: InputProps) {
+
+  function handleInputChange(event: React.ChangeEvent<HTMLInputElement>) {
+    const newValue = event.target.value;
+    onChangeInput?.(newValue);
+  }
+  return (
+    <Flex
+      alignItems="center"
+      flexDirection="row"
+      justifyContent="space-between"
+      bg="rgb(247, 248, 250)"
+      p="1rem 1rem 1.7rem"
+      borderRadius="1.25rem" border="0.06rem solid rgb(237, 238, 242)"
+      _hover={{ border: "0.06rem solid rgb(211,211,211)" }}>
+      <Box
+        flexDirection="row"
+        display="flex"
+        width="100%"
+      >
+        <Box>
+          <Text
+            color="black"
+            fontSize="30"
+            pl="5px"
+            fontWeight="500">
+            {tokenName}
+          </Text>
+        </Box>
+        <Box>
+          <Input
+            placeholder="0.0"
+            fontWeight="500"
+            fontSize="1.5rem"
+            width="100%"
+            size="19rem"
+            textAlign="right"
+            bg="rgb(247, 248, 250)"
+            outline="none"
+            border="none"
+            focusBorderColor="none"
+            type="number"
+            color="black"
+            value={value}
+            onChange={handleInputChange}
+          />
+        </Box>
+      </Box>
+    </Flex>
+  )
+}
